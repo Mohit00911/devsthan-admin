b4320b461dea35f39a2f3585cfdce9ffed79479b
Mohit Rawat <91361739+Mohit00911@users.noreply.github.com>
2024-10-18 21:08:32 +0530

    Update headers.js

--- src/utils/headers.js
+++ src/utils/headers.js
@@ -1,6 +1,6 @@
 const PORT = 4000
 
-const BASE_URL=`https://devsthan-backend-bsqm.vercel.app`
+const BASE_URL=`https://devsthan-backend-w2cmaskmc.vercel.app`
 const VERSEL_URL="https://devsthan.vercel.app"
 
 
